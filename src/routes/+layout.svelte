f676e3d6d4fb5274b5259fb71cdda53e058d7ee4
dannydeeps <contactdanielhuggins@gmail.com>
2023-10-16 21:04:10 +0100

    setup sveltestrap and create basic homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,33 @@
 <script>
 	import '../app.scss';
+
+	import {
+		Collapse,
+		Navbar,
+		Nav,
+		NavbarBrand,
+		NavbarToggler,
+		NavItem,
+		NavLink
+	} from 'sveltestrap';
+
+	let isOpen = false;
+
+	function handleUpdate(event) {
+		isOpen = event.detail.isOpen;
+	}
 </script>
+
+<Navbar container="md" class="bg-body-tertiary" expand="md">
+	<NavbarBrand href="/">Get Money</NavbarBrand>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)}/>
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav navbar>
+			<NavItem>
+				<NavLink href="/apply">Apply Now</NavLink>
+			</NavItem>
+		</Nav>
+	</Collapse>
+</Navbar>
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,27 @@
-<div class="container">
-	<div class="row">
-		<div class="col-12">
-			<div class="fs-1">Homepage</div>
-		</div>
-	</div>
-</div>
+<script>
+	import { Button, Col, Container, NavLink, Row } from "sveltestrap";
+</script>
+
+<Container md>
+	<Row class="gy-5">
+		<Col xs="12">
+			<p class="fs-1 fw-bold">Get loans from £10k to £25k</p>			
+			<p class="fs-2">See if your accepted without hurting your credit score!</p>
+			<div class="fs-4">
+				<strong>Choice</strong> - compare 100s of loans
+			</div>
+			
+			<div class="fs-4">
+				<strong>Fast</strong> - response in seconds
+			</div>
+			
+			<div class="fs-4">
+				<strong>No fuss</strong> - online approvals
+			</div>
+			
+			<div class="mt-4">
+				<Button href="/apply" size="lg" color="success" style="min-width: 300px;">Apply Now</Button>
+			</div>
+		</Col>
+	</Row>
+</Container>
